9422729f638ff66736b93e71d423b1620170b145
Chitra Jagadeesan <chitra@irokopartners.com>
2015-10-08 22:04:35 +0000

    ROutes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,8 +4,5 @@
 $app->get('/vouchers', 'VouchersController@index');
 $app->post('/vouchers/bulk', 'VouchersController@bulkCreate');
 $app->post('/vouchers', 'VouchersController@create');
-$app->put('/vouchers/{voucher_id}', 'VouchersController@put');
+$app->put('/vouchers/{voucher_id}', 'VouchersController@update');
 $app->post('/vouchers/redeem', 'VouchersController@redeem');
-$app->post('/vouchers/redeem', 'VouchersController@update');
-
-
