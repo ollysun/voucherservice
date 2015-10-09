6b2f521646a5e536bdf3fe116b48c59456f5a4e3
Chizzy Alaedu <chizzy.alaedu@irokopartners.com>
2015-10-09 11:26:42 +0100

    Refactored Validator

--- app/Validators/VoucherValidator.php
+++ app/Validators/VoucherValidator.php
@@ -4,75 +4,102 @@ use Illuminate\Validation\Validator as IlluminateValidator;
 
 class VoucherValidator extends IlluminateValidator
 {
-    public static function getVoucherRules()
-    {
-        return [
-            'user_id' => 'regex:/(^[0-9]+$)+/|required',
-            'platform' => 'string|required',
-            'valid_form' => 'date|date_format:Y-m-d H:i:s',
-            'valid_to' => 'date|date_format:Y-m-d H:i:s|after:valid_from',
-            'status' => 'required|regex:/(^[0-9]+$)+/',
-            'title' => 'required|regex:/(^[0-9]+$)+/',
-            'description' => 'required|string',
-            'location' => 'required|string',
-            'is_limited' => 'required|boolean',
-            'limit' => 'required|integer',
-            'period' => 'required|regex:/(^[0-9]+$)+/',
-            'duration' => 'required|regex:/(^[0-9]+$)+/',
-            'category' => 'required|regex:/(^[0-9]+$)+/',
-            'type' => 'required|integer',
-            'code' => 'required|string'
-        ];
-    }
-
-    public static function getIdRules()
-    {
-        return [
-            'id' => 'required|regex:/^[0-9]+$/'
-        ];
-    }
-
     public static function getParamsRules()
     {
         return [
             'query' => 'string',
-            'sort' => 'in:created_at,updated_at,user_id,is_active,status',
+            'sort' => 'in:created_at,updated_at,user_id,type,status,category,is_limited,valid_from,valid_to',
             'limit' => 'regex:/(^[0-9]+$)+/',
             'order' => 'in:ASC,DESC',
             'offset' =>'regex:/(^[0-9]+$)+/'
         ];
     }
 
+    public static function getVoucherRules()
+    {
+        return [
+            'type' => 'required|string|in:time,discount',
+            'status' => 'required|string|in:active,claiming,claimed,deleted,expired,in_active',
+            'category' => 'required|string|in:new,expired,active,new_expired',
+            'title' => 'required|string|in:Internal,VODAFONE_GHANA_STAFF_MOBILE,VODAFONE_GHANA_CUSTOMER_MOBILE,VODAFONE_GHANA_STAFF_FIXEDLINE',
+            'location' => 'required|string',
+            'description' => 'required|string',
+            'duration' => 'required||regex:/(^[0-9]+$)+/',
+            'period' => 'required|string|in:day,week,month,year',
+            'valid_from' => 'date|date_format:Y-m-d H:i:s',
+            'valid_to' => 'date|date_format:Y-m-d H:i:s|after:valid_from',
+            'is_limited' => 'required|boolean',
+            'limit' => 'regex:/(^[0-9]+$)+/'
+        ];
+    }
+
+    public static function getIdRules()
+    {
+        return [
+            'id'=> 'required|regex:/(^[0-9]+$)+/',
+        ];
+    }
+
+    public static function getUserIdRules()
+    {
+        return [
+            'user_id'=> 'required|regex:/(^[0-9]+$)+/',
+        ];
+    }
+
+    public static function getRedeemRules()
+    {
+        return [
+            'user_id'=> 'required|regex:/(^[0-9]+$)+/',
+            'platform' => 'required|string|in:web,cms,mobile',
+            'code' => 'required|string'
+        ];
+    }
+
     public static function getMessages()
     {
         return [
-            'platform.required' => 'The platform required.',
-            'platform.string' => 'The platform can on be a string.',
-            'user_id.regex' => 'The User Id can only be number',
-            'user_id.required' => 'The User id is required.',
-            'valid_from.date_format' => 'Provide the voucher valid From date',
-            'valid_to.date_format' => 'Provide the voucher valid To date',
-            'status.required' => 'Voucher status is required',
-            'status.regex' => 'Voucher status can only be number',
-            'title.required' => 'Voucher title is required',
-            'title.string' => 'Voucher title can only be string',
-            'description.string' => 'Voucher description can only be string',
-            'location.string' => 'Provide the Voucher location',
-            'is_limit.boolean' => 'Provide the is_limit condition',
-            'limit.integer' => 'Provide the limit number',
-            'period.regex' => 'Provide the period for the voucher',
-            'duration.integer' => 'Specify the Voucher duration',
-            'category.integer' => 'Provide the category status',
-            'code.required' => 'Provide the voucher code',
-            'code.string' => 'Voucher code can only be string',
-            'type.integer' => 'Provide the Voucher Type',
-            'id.required' => 'The voucher Id is required',
-            'id.regex' => 'Voucher Id can only be integer.',
+            'query.string'   =>  'The search query can only contain numbers or alphabets.',
+            'sort.in'    =>  'The sort parameter is invalid.',
             'limit.regex' =>  'Pagination limit must be an integer.',
             'order.in' => 'order can only be ASC or DESC',
             'offset.regex'    =>  'Pagination offset must be an integer.',
-            'query.string'   =>  'The search query can only contain numbers or alphabets.',
-            'sort.in'    =>  'The sort parameter is invalid.'
+            'type.required' => 'The Voucher Type is required',
+            'type.string' => 'The Voucher Type can only be a string',
+            'type.in' => 'The Voucher Type is invalid',
+            'status.required' => 'The Voucher Status is required',
+            'status.string' => 'The Voucher Status can only be a string',
+            'status.in' => 'The Voucher Status is invalid',
+            'category.required' => 'The Category Status is required',
+            'category.string' => 'The Category Status can only be a string',
+            'category.in' => 'The Category Status is invalid',
+            'title.required' => 'The Voucher Title is required',
+            'title.string' => 'The Voucher Title can only be a string',
+            'title.in' => 'The Voucher Title is invalid',
+            'location.required' => 'The Voucher Location is required',
+            'location.string' => 'The Voucher Location can only be a string',
+            'description.required' => 'The Voucher Description is required',
+            'description.string' => 'The Voucher Description can only be a string',
+            'duration.required' => 'The Voucher Duration is required',
+            'duration.regex' => 'The Voucher Duration can only be a number',
+            'period.required' => 'The Voucher Period is required',
+            'period.string' => 'The Voucher Period can only be a string',
+            'period.in' => 'The Voucher Period is invalid',
+            'valid_from.date_format' => 'Provide the Voucher valid from date',
+            'valid_to.date_format' => 'Provide the Voucher valid to date',
+            'is_limited.required' => 'The Voucher is-limited is required',
+            'is_limited.boolean' => 'The Voucher is-limited must be boolean',
+            'id.required' => 'The Voucher Id is required',
+            'id.regex' => 'The Voucher Id can only be an integer',
+            'id.exists'  =>  'The specified Voucher Id does not exist',
+            'user_id.required' => 'The User Id is required',
+            'user_id.regex' => 'The User Id can only be an integer',
+            'user_id.exists'  =>  'The specified User Id does not exist',
+            'platform.required' => 'The Platform is required',
+            'platform.string' => 'The Platform can only be a string',
+            'platform.in' => 'The Platform is invalid',
+            'code.required' => 'The Code is required',
+            'code.string' => 'The Code can only be a string'
         ];
     }
 }
