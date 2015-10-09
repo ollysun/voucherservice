acdd2d68a7be7b4c9d91ec882c8bdacbf44cf620
Chizzy Alaedu <chizzy.alaedu@irokopartners.com>
2015-10-09 10:56:56 +0100

    Added Log Route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,3 +6,6 @@ $app->post('/vouchers/bulk', 'VouchersController@bulkCreate');
 $app->post('/vouchers', 'VouchersController@create');
 $app->put('/vouchers/{voucher_id}', 'VouchersController@update');
 $app->post('/vouchers/redeem', 'VouchersController@redeem');
+
+// Voucher Log routes
+$app->get('/vouchers/{user_id}/logs', 'VouchersController@showByUserId');
