f9ff380d2089d39d5722519c497c6a816f0951c3
Chizzy Alaedu <chizzy.alaedu@irokopartners.com>
2015-10-12 12:52:31 +0100

    Added Voucher Code Model

---
+++ app/Models/VoucherCode.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tech7
+ * Date: 10/12/15
+ * Time: 12:48 PM
+ */
+
+namespace Voucher\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VoucherCode extends Model
+{
+
+}
