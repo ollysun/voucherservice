4af958e1a4223110438a563b05c6e050e753f505
Chizzy Alaedu <chizzy.alaedu@irokopartners.com>
2015-10-16 16:49:31 +0100

    Added test for VoucherCodesRepository
    * Pass and Fail test for existing voucher

--- tests/Repositories/VoucherCodesRepositoryTest.php
+++ tests/Repositories/VoucherCodesRepositoryTest.php
@@ -1,6 +1,48 @@
 <?php
 
+use Voucher\Models\VoucherCode;
+use Voucher\Repositories\VoucherCodesRepository;
+
 class VoucherCodesRepositoryTest extends TestCase
 {
+    protected $model;
+    protected $repository;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->model = new VoucherCode();
+        $this->repository = new VoucherCodesRepository($this->model);
+    }
+
+    public function testIsNotExistingVoucherCode()
+    {
+        $result = $this->repository->isNotExistingVoucherCode('xxxxxxxx9');
+        $this->assertTrue($result);
+    }
+
+    public function testIsNotExistingVoucherCodeFalse()
+    {
+        $data = [
+            'voucher_status' => 'new',
+            'voucher_code' => 'abc123'
+        ];
+
+        $code = $this->repository->insertVoucherCode($data);
+        $this->assertNotNull($code);
+
+        $result = $this->repository->isNotExistingVoucherCode('abc123');
+        $this->assertFalse($result);
+    }
+
+    public function testIsNotExistingVoucherCodeErrorException()
+    {
+//        $this->setExpectedException('\Exception');
+
+        $this->model = $this->getMock('Voucher\Models\VoucherCodes', ['where']);
+        $this->model->expects($this->any())->method('where')->willThrowException(new \Exception());
+
+        $this->repository = new VoucherCodesRepository($this->model);
+        $this->repository->isNotExistingVoucherCode('xxxxxxxx9');
+    }
 }
