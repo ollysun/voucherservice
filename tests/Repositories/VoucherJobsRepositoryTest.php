a29e6d7992b5e7a926dae39fb723b4c60c49af35
Chizzy Alaedu <chizzy.alaedu@irokopartners.com>
2015-10-19 08:03:32 +0100

    Added getJobs test

--- tests/Repositories/VoucherJobsRepositoryTest.php
+++ tests/Repositories/VoucherJobsRepositoryTest.php
@@ -1,6 +1,38 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
+use Voucher\Models\VoucherJob;
+use Voucher\Repositories\VoucherJobsRepository;
+
 class VoucherJobsRepositoryTest extends TestCase
 {
+    protected $model;
+    protected $repository;
 
+    public function setUp()
+    {
+        parent::setUp();
+        $this->model = new VoucherJob();
+        $this->repository = new VoucherJobsRepository($this->model);
+    }
+
+    public function testGetJobs()
+    {
+        DB::table('voucher_jobs')
+            ->insert([
+                'status' => 'new',
+                'comments' => 'a comment'
+            ]);
+
+        $result = $this->repository->getJobs();
+        $this->assertNotNull($result['data'][0]['id']);
+    }
+
+//    public function testGetJobsFalse()
+//    {
+//        $this->model->truncate();
+//
+//        $result = $this->repository->getJobs();
+//        $this->assertEmpty($result);
+//    }
 }
