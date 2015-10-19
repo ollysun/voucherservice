69fd4bbd363ae92c56343b0d80b5c3b2430ebb2c
Chizzy Alaedu <chizzy.alaedu@irokopartners.com>
2015-10-19 17:25:55 +0100

    Test for getVoucherById and getVoucherByCode

--- tests/Repositories/VouchersRepositoryTest.php
+++ tests/Repositories/VouchersRepositoryTest.php
@@ -153,4 +153,120 @@ class VouchersRepositoryTest extends TestCase
         );
         $this->repository->getVouchers($params);
     }
+
+    public function testGetVoucherById()
+    {
+        $voucher_job_model = new \Voucher\Models\VoucherJob();
+        $voucher_job_model->insert(['id' => 9990, 'status' => 'new', 'comments' => 'a comment']);
+
+        $data = [
+            'id' => 9999,
+            'code' => '12345678abc',
+            'type' => 'time',
+            'status' => 'active',
+            'category' => 'new',
+            'title' => 'INTERNAL',
+            'location' => 'Nigeria',
+            'description' => 'description',
+            'duration' => 3,
+            'period' => 'day',
+            'valid_from' => '2015-10-08 00:00:00',
+            'valid_to' => '2015-12-30 00:00:00',
+            'is_limited' => 0,
+            'limit' => 2,
+            'voucher_job_id' => 9990
+        ];
+        $this->voucher_model->insert($data);
+
+        $this->voucher_log_model->insert([
+            'voucher_id' => 9999,
+            'user_id' => '9999a',
+            'action' => 'attempt',
+            'platform' => 'mobile',
+            'comments' => 'a comment'
+        ]);
+
+        $result = $this->repository->getVoucherById('9999');
+        $this->assertEquals($data['id'], $result['data']['id']);
+        $this->assertEquals($data['code'], $result['data']['code']);
+    }
+
+    public function testGetVoucherByIdNull()
+    {
+        $result = $this->repository->getVoucherById('9999a');
+        $this->assertNull($result);
+    }
+
+    public function testGetVoucherByIdErrorException()
+    {
+        $this->setExpectedException('\Exception');
+        $this->voucher_model = $this->getMock(Voucher::class, ['where']);
+        $this->voucher_model->expects($this->any())->method('where')->willThrowException(new \Exception());
+
+        $this->repository = new VouchersRepository(
+            $this->voucher_model,
+            $this->voucher_log_model,
+            $this->voucher_param_model,
+            $this->voucher_code_model
+        );
+        $this->repository->getVoucherById('xyz123');
+    }
+
+    public function testGetVoucherByCode()
+    {
+        $voucher_job_model = new \Voucher\Models\VoucherJob();
+        $voucher_job_model->insert(['id' => 9990, 'status' => 'new', 'comments' => 'a comment']);
+
+        $data = [
+            'id' => 9999,
+            'code' => '12345678abc',
+            'type' => 'time',
+            'status' => 'active',
+            'category' => 'new',
+            'title' => 'INTERNAL',
+            'location' => 'Nigeria',
+            'description' => 'description',
+            'duration' => 3,
+            'period' => 'day',
+            'valid_from' => '2015-10-08 00:00:00',
+            'valid_to' => '2015-12-30 00:00:00',
+            'is_limited' => 0,
+            'limit' => 2,
+            'voucher_job_id' => 9990
+        ];
+        $this->voucher_model->insert($data);
+
+        $this->voucher_log_model->insert([
+            'voucher_id' => 9999,
+            'user_id' => '9999a',
+            'action' => 'attempt',
+            'platform' => 'mobile',
+            'comments' => 'a comment'
+        ]);
+
+        $result = $this->repository->getVoucherByCode('12345678abc');
+        $this->assertEquals($data['code'], $result['data']['code']);
+        $this->assertEquals($data['id'], $result['data']['id']);
+    }
+
+    public function testGetVoucherByCodeFalse()
+    {
+        $result = $this->repository->getVoucherByCode('123456789abc');
+        $this->assertFalse($result);
+    }
+
+    public function testGetVoucherByCodeErrorException()
+    {
+        $this->setExpectedException('\Exception');
+        $this->voucher_model = $this->getMock(Voucher::class, ['where']);
+        $this->voucher_model->expects($this->any())->method('where')->willThrowException(new \Exception());
+
+        $this->repository = new VouchersRepository(
+            $this->voucher_model,
+            $this->voucher_log_model,
+            $this->voucher_param_model,
+            $this->voucher_code_model
+        );
+        $this->repository->getVoucherByCode('123456789abc');
+    }
 }
