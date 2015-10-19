56bc140a7834793f9436cba72802fca9d410b3e6
Lawrence Enehizena <lawrence.enehizena@irokopartners.com>
2015-10-19 14:01:17 +0100

    voucher test- more cases

--- tests/Voucher/VoucherTest.php
+++ tests/Voucher/VoucherTest.php
@@ -31,14 +31,14 @@ class VoucherTest extends TestCase
     {
         $this->voucher_repo->create([
             'valid_from' => '2015-1-1 20:11:1',
-            'valid_to' => '2015-12-1 20:11:1',
+            'valid_to' => date('Y-m-d H:i:s', strtotime('+ 1 day')),
             'limit' => 1,
             'type' => 'time',
-            'code' => 'XD34E1'
+            'code' => 'XAD34E1'
         ]);
 
         $inputs = [
-          'code' => 'XD34E1',
+          'code' => 'XAD34E1',
             'user_id' => 122333,
             'platform' => 1,
         ];
@@ -49,19 +49,18 @@ class VoucherTest extends TestCase
         $this->assertEquals(true, $result);
     }
 
-
-    public function testRedeemWithException()
+    public function testRedeemWithNoneExistingCodeException()
     {
         $this->voucher_repo->create([
             'valid_from' => '2015-1-1 20:11:1',
-            'valid_to' => '2015-12-1 20:11:1',
+            'valid_to' => date('Y-m-d H:i:s', strtotime('+ 1 day')),
             'limit' => 1,
             'type' => 'time',
-            'code' => 'XD34E1QQ'
+            'code' => 'XCD34E1QQ'
         ]);
 
         $inputs = [
-            'code' => 'XD34E1',
+            'code' => 'XCD34E1',
             'user_id' => 122333,
             'platform' => 1,
         ];
@@ -71,12 +70,84 @@ class VoucherTest extends TestCase
         $voucher->setSubscriptionService(new \Voucher\Services\SubscriptionService());
         $voucher->redeem($inputs);
     }
-//
-//    public function testIsVoucherExistsAndValid()
-//    {
-//
-//    }
-//
+
+    public function testRedeemWithUsedCodeException()
+    {
+        $this->voucher_repo->create([
+            'valid_from' => '2015-1-1 20:11:1',
+            'valid_to' => date('Y-m-d H:i:s', strtotime('+ 1 day')),
+            'limit' => 1,
+            'type' => 'time',
+            'status' => 'inactive',
+            'code' => 'XD34E1HH'
+        ]);
+
+        $inputs = [
+            'code' => 'XD34E1HH',
+            'user_id' => 122333,
+            'platform' => 1,
+        ];
+
+        $this->setExpectedException('\Exception');
+        $voucher =  new VoucherService($this->voucher_repo, $this->voucher_log_repo);
+        $voucher->setSubscriptionService(new \Voucher\Services\SubscriptionService());
+        $voucher->redeem($inputs);
+    }
+
+    public function testRedeemWithExpiredCodeException()
+    {
+        $this->voucher_repo->create([
+            'valid_from' => '2015-1-1 20:11:1',
+            'valid_to' => '2014-1-1 20:11:1',
+            'limit' => 1,
+            'type' => 'time',
+            'status' => 'active',
+            'code' => 'XD34E1SS'
+        ]);
+
+        $inputs = [
+            'code' => 'XD34E1SS',
+            'user_id' => 122333,
+            'platform' => 1,
+        ];
+
+        $this->setExpectedException('\Exception');
+        $voucher =  new VoucherService($this->voucher_repo, $this->voucher_log_repo);
+        $voucher->setSubscriptionService(new \Voucher\Services\SubscriptionService());
+        $voucher->redeem($inputs);
+    }
+
+    public function testRedeemWithUsageLimitReachedException()
+    {
+        $inputs = [
+            'code' => 'XD34E1Q11',
+            'user_id' => 122333,
+            'platform' => 1,
+        ];
+
+        $voucher_insert = $this->voucher_repo->create([
+            'valid_from' => '2015-1-1 20:11:1',
+            'valid_to' => date('Y-m-d H:i:s', strtotime('+ 1 day')),
+            'limit' => 1,
+            'type' => 'time',
+            'status' => 'active',
+            'code' => 'XD34E1Q11'
+        ]);
+
+        $this->voucher_log_repo->addVoucherLog([
+            'voucher_id' => $voucher_insert['data']['id'],
+            'user_id' => $inputs['user_id'],
+            'action' => 'success',
+            'platform' => 'mobile',
+            'comments' => 'just test',
+        ]);
+
+        $this->setExpectedException('\Exception');
+        $voucher =  new VoucherService($this->voucher_repo, $this->voucher_log_repo);
+        $voucher->setSubscriptionService(new \Voucher\Services\SubscriptionService());
+        $voucher->redeem($inputs);
+    }
+
 //    public function testIsVoucherExistsAndValidWithException()
 //    {
 //
