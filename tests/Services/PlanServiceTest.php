6d3b92adf2de9ba42a31b8b9c7a5082a80d2a647
Lawrence Enehizena <lawrence.enehizena@irokopartners.com>
2015-10-21 11:24:31 +0100

    added plan and subscription service test case

---
+++ tests/Services/PlanServiceTest.php
@@ -0,0 +1,25 @@
+<?php
+use Voucher\Services\PlanService;
+
+class PlanServiceTest extends TestCase
+{
+    protected $plan_service;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->plan_service = new PlanService();
+    }
+
+    public function testPlansApi()
+    {
+        $result = $this->plan_service->plansApi('/plans/1', 'get');
+        $this->assertArrayHasKey('data', $result);
+    }
+
+    public function testPlanApiWithInternalErrorException()
+    {
+        $result = $this->plan_service->plansApi('/plans/10000000000000000', 'get');
+        $this->assertEquals(false, $result);
+    }
+}
